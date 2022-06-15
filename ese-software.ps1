ccd35747af79efde61ee7d65bb0bcbabc1a1426f
l3043y <boreyc2013@gmail.com>
2022-06-15 10:04:44 +0700

    updates

--- ese-software.ps1
+++ ese-software.ps1
@@ -1,17 +1,14 @@
 choco install pdfxchangeeditor -y
 choco install sourcetree -y
 choco install thunderbird -y
-
 choco install kicad --version=5.1.5.20 -y
 choco install freecad --version=0.18.16146 -y
 choco install telegram.install -y
 choco install drawio -y
 choco install visualstudio2022community -y
 choco install chocolateygui -y
-
 choco install googlechrome -y
-
 choco install autodesk-fusion360 -y
 choco install notepadplusplus.install -y
-
 choco install vscode -y
+choco install javaruntime -y
